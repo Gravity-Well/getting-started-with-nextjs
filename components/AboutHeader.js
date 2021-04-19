0877afc09cc5a83acfebe86f6f3f7d0067e7b92b
robert <bobtomlin@gmail.com>
2021-04-19 15:24:06 -0400

    first commit

---
+++ components/AboutHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutHeader() {
+    return (
+        <div>
+               <h1>My About Page Header</h1>
+               <hr/>
+        </div>
+    )
+}
+
+export default AboutHeader
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AboutHeader from '../components/AboutHeader'
+
+function about() {
+    return (
+        <div>
+            <AboutHeader/>
+            <h2>About My Application</h2>
+        </div>
+    )
+}
+
+export default about
